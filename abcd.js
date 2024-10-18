d29749f1e8a4d53f8ffc0782b6bcaae6fee1265e
VAsi45678 <vsri6414@gmail.com>
2024-10-18 12:11:11 +0530

    hello

---
+++ abcd.js
@@ -0,0 +1,16 @@
+if (hour<18){
+    greeting ="Good day";
+
+}
+
+if (hour<18){
+    greeting ="Good day";
+
+} else {
+    greeting ="Good evening"
+var b=5
+    console.log(Math.Sqrt2(b))
+
+    let myFunction =(a,b) a*b; 
+    
+    hello =function()
